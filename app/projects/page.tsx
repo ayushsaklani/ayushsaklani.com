ed69a52f53eac0c1c971c3104b6f755b3e066e5b
Ayush Saklani <saklaniayush99@gmail.com>
2024-09-09 19:02:48 -0700

    Rectified Lint Errors

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -17,8 +17,8 @@ export default async function ProjectPage() {
   return (
     <div className="relative w-full grid grid-cols-1 md:grid-cols-2 gap-5 px-5 pt-10 mt-10 pb-[8rem]">
       {ProjectList.map((project:ProjectProps) => (
-        <a href={project.url[0].url} target="_blank" className="">
-         <ProjectCard key={project.id} className="w-full bg-slate-800 p-2 text-slate-300" project={project}/>
+        <a key={project.id} href={project.url[0].url} target="_blank" className="">
+         <ProjectCard  className="w-full bg-slate-800 p-2 text-slate-300" project={project}/>
         </a>    
       ))}
 

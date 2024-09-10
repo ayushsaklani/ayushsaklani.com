542431bd6b931ef00acb5d0b5fac5a4b3cc5b669
Ayush Saklani <saklaniayush99@gmail.com>
2024-09-09 18:12:41 -0700

    Added anchor to project card

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -17,8 +17,9 @@ export default async function ProjectPage() {
   return (
     <div className="relative w-full grid grid-cols-1 md:grid-cols-2 gap-5 px-5 pt-10 mt-10 pb-[8rem]">
       {ProjectList.map((project:ProjectProps) => (
-       <ProjectCard key={project.id} className="w-full bg-slate-800 p-2 text-slate-300" project={project}/>
-      
+        <a href={project.url[0].url} target="_blank" className="">
+         <ProjectCard key={project.id} className="w-full bg-slate-800 p-2 text-slate-300" project={project}/>
+        </a>    
       ))}
 
     </div>

b32025e9ac1284989559fb15d86802a848562ad8
Ayush Saklani <saklaniayush99@gmail.com>
2024-09-10 22:20:39 -0700

    Removed async from Project page.tsx

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -5,15 +5,8 @@ import { ProjectList } from "@/data/projects";
 import ProjectCard from "@/components/projectCard/ProjectCard";
 import { ProjectProps } from "@/types";
 
-export default async function ProjectPage() {
+export default function ProjectPage() {
 
-    //  const endpoint = "https://strapi.saklanicloud.com";
-	
-
-    // const data = await fetch(endpoint+'/api/projects?populate=*&sort=done_on:desc')
-    // let projects = await data.json()
-    // projects = projects.data
-  // console.log(projects);
   return (
     <div className="relative w-full grid grid-cols-1 md:grid-cols-2 gap-5 px-5 pt-10 mt-10 pb-[8rem]">
       {ProjectList.map((project:ProjectProps) => (
